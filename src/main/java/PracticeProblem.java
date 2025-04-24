80965853f32e2924be57d7b880d2d98da12ad981
Aathithyan-Thavachselvan-ics4u1-03-25 <aathithyan.thavachselvan@student.tdsb.on.ca>
2025-04-24 01:19:41 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,45 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		searchMazeMoves(new String[][]{
+			{"", "", "", "", ""},
+			{"", "*", "", "", ""},
+			{"", "*", "", "*", ""},
+			{"S", "*", "", "", "F"},
+		  });
 	}
-
-	public static void q1() {
-		//Write question 1 code here
+	public static int searchMazeMoves(String[][] arr){
+		int row=arr.length-1;
+		int col=0;
+		int noOfMoves=0;
+		return searchMazeHelper(row, col, noOfMoves, arr);
 	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	public static int searchMazeHelper(int row, int col, int moves, String[][] arr){
+		
+		if(row<0||row>arr.length-1 || col>arr[row].length-1 ||col<0|| arr[row][col].equals("*")){
+			return -1;
+		}
+		if(arr[row][col].equals("F")){
+			return moves;
+		}
+		String copy = arr[row][col];
+		arr[row][col]= "*";
+		int up = searchMazeHelper(row-1, col, moves+1, arr);
+		int down = searchMazeHelper(row+1, col, moves+1, arr);
+		int right = searchMazeHelper(row, col+1, moves+1, arr);
+		int left = searchMazeHelper(row, col-1, moves+1, arr);
+		arr[row][col]=copy;
+		int[] num={up, down, right, left};
+		int smallest=Integer.MAX_VALUE;
+		for(int i=0; i<num.length; i++){
+			if(smallest>num[i]&& num[i]>=0){
+				smallest =num[i];
+			}
+		}
+		if(smallest==Integer.MAX_VALUE){
+			return -1;
+		}
+		return smallest;
 	}
 
 }
